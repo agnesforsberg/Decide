396a6c002441193295d74aba4bee37ca80fc06bb
JakobBerggren <jaberggr@kth.se>
2021-01-29 11:32:47 +0100

    point class added

---
+++ src/main/Point.java
@@ -0,0 +1,13 @@
+package Decide.src.main;
+
+public class Point {
+    
+    //X and Y coordinates
+    public double x;
+    public double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+}
