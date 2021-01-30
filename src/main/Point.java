cb0ea505d1c59f16a02e7510cac1afdee009bdb8
Caroline <cawibo@gmail.com>
2021-01-30 14:47:14 +0100

    Improve upon the skeleton in issue#17
    This patch reformats some code according to Java code specification and
    adds documentation where it was missing. It also implements a
    distanceTo-method in the Point class and corresponding tests.
    
    It also renames the Main-class (main.java -> Main.java).

--- src/main/Point.java
+++ src/main/Point.java
@@ -1,13 +1,31 @@
 package Decide.src.main;
 
+import java.lang.Math;
+
+/**
+ * A class representing a point in 2-D space with double accuracy.
+ */
 public class Point {
-    
-    //X and Y coordinates
+
+    // X and Y coordinates
     public double x;
     public double y;
 
-    public Point(double x, double y){
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    /**
+     * Returns the distance between two Points.
+     * 
+     * @param other another Point to measure the distance to.
+     * @return the Euclidean distance between this Point and the other Point.
+     */
+    public double distanceTo(Point other) {
+        double xDifference = this.x - other.x;
+        double yDifference = this.y - other.y;
+
+        return Math.sqrt(Math.pow(xDifference, 2) + Math.pow(yDifference, 2));
     }
 }
