7e1b3c70e33150442e0b58c03c21060ad2d4b102
Agnes <agnesfo@kth.se>
2021-02-02 15:27:37 +0100

    issue#14 done

--- src/main/LaunchInterceptor.java
+++ src/main/LaunchInterceptor.java
@@ -287,8 +287,25 @@ public class LaunchInterceptor {
         return false;
     }
 
-    boolean lic7() {
-        return true;
+    /**
+     * There exists at least one set of two data points separated by exactly K PTS consecutive
+     * intervening points that are a distance greater than the length, LENGTH1, apart. The condition
+     * is not met when NUMPOINTS < 3
+     * @return whether the LIC7 condition holds or not.
+     */
+    public boolean lic7() {
+        if (numPoints < 3)
+            return false;
+
+        for (int i = 0; i < numPoints - parameters.K_PTS - 1; i++) {
+            Point a = points[i];
+            Point b = points[i + parameters.K_PTS + 1];
+
+            if (a.distanceTo(b) > parameters.LENGTH1 )
+                return true;
+        }
+
+        return false;
     }
 
 
@@ -371,8 +388,27 @@ public class LaunchInterceptor {
         return false;
     }
 
-    boolean lic12() {
-        return true;
+    /**
+     * There exists at least one set of two data points, separated by exactly K PTS consecutive
+     * intervening points, which are a distance greater than the length, LENGTH1, apart. In addition,
+     * there exists at least one set of two data points (which can be the same or different from
+     * the two data points just mentioned), separated by exactly K PTS consecutive intervening
+     * points, that are a distance less than the length, LENGTH2, apart. Both parts must be true
+     * for the LIC to be true. The condition is not met when NUMPOINTS < 3.
+     * @return whether the LIC12 condition holds or not.
+     */
+    public boolean lic12() {
+        if (!lic7())
+            return false;
+
+        for (int i = 0; i < numPoints - parameters.K_PTS - 1; i++) {
+            Point a = points[i];
+            Point b = points[i + parameters.K_PTS + 1];
+
+            if (a.distanceTo(b) < parameters.LENGTH2 )
+                return true;
+        }
+        return false;
     }
 
     /**
