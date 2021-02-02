e2aa103397778529d16e77998480bc8df748c5d0
JakobBerggren <jaberggr@kth.se>
2021-02-02 17:57:00 +0100

    initial test, WIP

---
+++ src/test/LaunchInterceptorCondition14Test.java
@@ -0,0 +1,34 @@
+package Decide.src.test;
+
+import org.junit.Test;
+
+import java.util.Random;
+
+import Decide.src.main.Point;
+import Decide.src.main.LaunchInterceptor;
+import Decide.src.main.Parameters;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class LaunchInterceptorCondition14Test {
+    private Parameters parameters;
+    private LaunchInterceptor li;
+
+    private Random rand = new Random();
+
+    @Test
+    public void noPointsTest() {
+
+    }
+
+    @Test
+    public void trivialFailingTest() {
+        
+    }
+
+    @Test
+    public void trivialPassingTest() {
+        
+    }
+}
