d5f461f3a5b0f4e11ef776621353febe508bbb4e
agnesforsberg <66953501+agnesforsberg@users.noreply.github.com>
2021-02-03 09:43:08 +0100

    issue #62 comments added for tests for LIC2, 7 and 12 (#63)
    Co-authored-by: Agnes <agnesfo@kth.se>

--- src/test/LaunchInterceptorCondition2Test.java
+++ src/test/LaunchInterceptorCondition2Test.java
@@ -15,6 +15,9 @@ public class LaunchInterceptorCondition2Test {
 
     private Random rand = new Random();
 
+    /**
+     * Tests that LIC2 returns false when there are less than 3 points.
+     */
     @Test
     public void notEnoughPointsTest() {
         parameters = new Parameters();
@@ -29,6 +32,9 @@ public class LaunchInterceptorCondition2Test {
         assertFalse("LIC2 must be false if there are less than three points.", li.lic2());
     }
 
+    /**
+     * Tests that LIC2 returns false when there are no points.
+     */
     @Test
     public void noPointsTest() {
         parameters = new Parameters();
@@ -43,6 +49,11 @@ public class LaunchInterceptorCondition2Test {
         assertFalse("LIC2 must be false if there are no points.", li.lic2());
     }
 
+    /**
+     * Tests a trivial failing test when the points form a line.
+     * Since EPSILON = 1, a failing test is when the lines create an angle that satisfies 2.14 < angle < 4.14
+     * A line has the angle PI so this test will fail.
+     */
     @Test
     public void trivialFailingTest() {
         parameters = new Parameters();
@@ -57,6 +68,11 @@ public class LaunchInterceptorCondition2Test {
         assertFalse(li.lic2());
     }
 
+    /**
+     * Tests a trivial passing test when the points form a 90 degree angle (PI/2 radians).
+     * For the test to pass (based on EPSILON = 1) the angle must satify angle < 2.14 OR angle > 4.14
+     * This angle is PI/2 and so this test should pass.
+     */
     @Test
     public void trivialPassingTest() {
         parameters = new Parameters();
@@ -71,6 +87,11 @@ public class LaunchInterceptorCondition2Test {
         assertTrue(li.lic2());
     }
 
+    /**
+     * Tests that points need to be in consecutive order.
+     * The points a, b, c form a narrow, passing angle when having b as the vertex.
+     * When c is the vertex the angle is close to being a line, and the angle should not pass LIC2.
+     */
     @Test
     public void nonConsecutiveFailTest () {
         parameters = new Parameters();
@@ -89,6 +110,10 @@ public class LaunchInterceptorCondition2Test {
         assertFalse(li.lic2());
     }
 
+    /**
+     * Tests that if any endpoint of the angle coincides with the vertex LIC2 should fail.
+     * In this test all point coincide.
+     */
     @Test
     public void aCoincidesWithBFail() {
         parameters = new Parameters();
@@ -103,6 +128,10 @@ public class LaunchInterceptorCondition2Test {
         assertFalse(li.lic2());
     }
 
+    /**
+     * Tests that it does not need to be the first 3 points that form the passing angle.
+     * abc form a line, while bcd form a 90 degree angle.
+     */
     @Test
     public void fourPointsConsecutiveTest() {
         parameters = new Parameters();
