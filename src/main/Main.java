068e0661564dd549ee348ce10f3692c76541bc6d
agnesforsberg <66953501+agnesforsberg@users.noreply.github.com>
2021-02-03 09:39:57 +0100

    issue#59 done (#60)
    Co-authored-by: Agnes <agnesfo@kth.se>

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,13 +9,21 @@ public class Main {
 
         Parameters parameters = new Parameters(2.0, 2.0, 2.0, 2.0, 1, 1, 1.0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1.0, 1.0, 1.0);
 
+        /**
+         * For testing purposes not all LICS must be part of the result.
+         * For all LICS: numtests = 15, PUV.length = 15, LCM = [15][15]
+         * For all non-tests, a constructor without the numTests parameter exists.
+         */
+
         Connector[][] LCM = new Connector[15][15];
         // Add data to LCM
 
         boolean[] PUV = new boolean[15];
         // Add data to PUV
 
-        LaunchInterceptor launch = new LaunchInterceptor(numpoints, points, parameters, LCM, PUV);
+        int numTests = 15;
+
+        LaunchInterceptor launch = new LaunchInterceptor(numpoints, points, parameters, LCM, PUV, numTests);
         System.out.print(launch.decide());
 
     }
